f4474e0084d8aae59dc2450c94e1df0b0d27b1a9
santiagopemo <santiagopemo@gmail.com>
2020-09-09 21:51:37 +0000

    updated app.py and index.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,6 +9,8 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 app.url_map.strict_slashes = False
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+host = getenv("HBNB_API_HOST") or '0.0.0.0'
+port = getenv("HBNB_API_PORT") or 5000
 
 
 @app.teardown_appcontext
@@ -18,8 +20,4 @@ def close_storage(self):
 
 
 if __name__ == "__main__":
-    app.run(
-        host=getenv('HBNB_API_HOST', default='0.0.0.0'),
-        port=getenv('HBNB_API_PORT ', default='5000'),
-        threaded=True
-    )
+    app.run(host=host, port=port, threaded=True)
