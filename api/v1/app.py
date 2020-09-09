364d50705b57069729e67a7ff1144a0fc3845993
santiagopemo <santiagopemo@gmail.com>
2020-09-09 19:16:37 +0000

    added files for task 4

---
+++ api/v1/app.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""app Module"""
+from flask import Flask, Blueprint, jsonify
+from api.v1.views import app_views
+from os import getenv
+from models import storage
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+
+
+@app.teardown_appcontext
+def teardown_storage(self):
+    """Calls storage.close()"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(
+        host=getenv('HBNB_API_HOST', default='0.0.0.0'),
+        port=getenv('HBNB_API_PORT ', default='5000'),
+        threaded=True
+    )
---
+++ api/v1/views/index.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""index Module"""
+from api.v1.views import app_views
+from flask import jsonify
+
+
+@app_views.route('/status', strict_slashes=False)
+def status():
+    """Returns a JSON with status: Ok"""
+    return jsonify({"status": "OK"})
