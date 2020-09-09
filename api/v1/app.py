273815f8d56299ecbda8887052dbba222de98c56
santiagopemo <santiagopemo@gmail.com>
2020-09-09 20:59:18 +0000

    added permissions

--- api/v1/app.py
+++ api/v1/app.py
@@ -19,5 +19,6 @@ def close_storage(self):
 if __name__ == "__main__":
     app.run(
         host=getenv('HBNB_API_HOST', default='0.0.0.0'),
-        port=int(getenv('HBNB_API_PORT ', default='5000'))
+        port=getenv('HBNB_API_PORT ', default='5000'),
+        threaded=True
     )
